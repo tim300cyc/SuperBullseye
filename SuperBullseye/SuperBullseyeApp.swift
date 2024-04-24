813b815e0fa3f19ae676f83ecfee33220f530fa7
Tim <tim300cyc@gmail.com>
2024-04-24 15:07:25 +0800

    Initial Commit

---
+++ SuperBullseye/SuperBullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperBullseyeApp.swift
+//  SuperBullseye
+//
+//  Created by PrickupDriver on 2024/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperBullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
